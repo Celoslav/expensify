69a11bc0eaeffc9f4577d48c1a015ede77c392af
Celoslav <lucicmladen83@gmail.com>
2017-11-17 11:53:27 +0100

    Sass in the house

--- src/app.js
+++ src/app.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Indecision from './components/Indecision';
-
+import './styles/styles.scss';
 
 ReactDOM.render(<Indecision />, document.getElementById('app'));
-
-// class OldSyntax {
-//     constructor() {
-//         this.name = 'Mike';
-//     }
-// }
-// const oldSyntax = new OldSyntax();
-// console.log(oldSyntax);
-
-// //------
-
-// class NewSyntax {
-//     name = 'Jen'
-// }
-
-// const newSyntax = new NewSyntax();
-// console.log(newSyntax);
