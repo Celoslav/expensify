c2e8c2ab535b9bb84a4753160dcce1462101db78
Celoslav <lucicmladen83@gmail.com>
2017-11-20 09:54:51 +0100

    404 page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -9,9 +9,35 @@ const ExpenseDashboardPage = () => (
         This is from my dashboard component    
     </div>
 );
+const AddExpensePage = () => (
+    <div>
+        This is from my add expense component    
+    </div>
+);
+const EditExpensePage = () => (
+    <div>
+        This is from my edit expense component    
+    </div>
+);
+const HelpPage = () => (
+    <div>
+        This is from my help page component
+    </div>
+);
+const NotFoundPage = () => (
+    <div>
+        404!   
+    </div>
+);
 const routes = (
     <BrowserRouter>
-        <Route path="/" component={ExpenseDashboardPage} />   
+    <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} /> 
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+    </Switch>
     </BrowserRouter>
 );
 
